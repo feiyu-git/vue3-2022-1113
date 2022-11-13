4eaf61b461f1d7017aa65f9d46ecec7c2e626b0d
terry <2462858146@qq.com>
2022-11-13 18:16:21 +0800

    初始化工程

---
+++ src/components/topNav/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="top-nav">
+    <router-link
+      v-for="(item, index) in routerList.routes"
+      :key="index"
+      :to="item.path"
+    >
+      {{ item.label }}
+    </router-link>
+  </div>
+</template>
+
+<script>
+import router, { routes } from '../../router';
+import { reactive } from 'vue';
+export default {
+  setup() {
+    const routerList = reactive({
+      routes,
+      num: router.length,
+    });
+    return {
+      // ...ref(routerList),
+      routerList,
+    };
+  },
+};
+</script>
+
+<style lang="less">
+.top-nav {
+  display: flex;
+  margin-bottom: 12px;
+  padding-left: 12px;
+  a {
+    margin-right: 22px;
+    text-decoration: none;
+    font-size: 18px;
+    color: #000;
+    &.router-link-active {
+      font-weight: bold;
+    }
+  }
+}
+</style>
